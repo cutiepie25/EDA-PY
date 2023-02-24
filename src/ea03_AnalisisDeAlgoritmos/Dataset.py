0227ddf443d71f331e4327c159f88627c4ff78bb
Jorge Londoño <jorge.londono@upb.edu.co>
2023-02-24 10:58:24 -0500

    Improved implementation of the Dataset

--- src/ea03_AnalisisDeAlgoritmos/Dataset.py
+++ src/ea03_AnalisisDeAlgoritmos/Dataset.py
@@ -4,7 +4,7 @@ import stddraw
 
 class Dataset:
 
-    def generateClusters(self, n: int, d: int):
+    def generateClusters2D(self, n: int):
         self.points = []
         self.labels = []
         sigma = 0.5
@@ -17,6 +17,25 @@ class Dataset:
                 self.labels.append(0)
             punto = ( random.gauss(mu,sigma), random.gauss(mu,sigma) ) 
             self.points.append(punto)
+
+
+    def gaussianPoint(centroid: list[float], sigma: float) -> list[float]:
+        point = [ random.gauss(centroid[i],sigma) for i in range(len(centroid)) ]
+        return point
+
+
+    def generateClusters(self, n: int, dim: int, labels: int):
+        assert labels<=2**dim, "Number of labels must be <= 2**dim"
+        self.points = []
+        self.labels = []
+        centroids = Dataset.hypercube(dim)
+        sigma = 0.5
+        for i in range(n):
+            label = random.randint(0,labels-1)
+            # print(label, centroids[label])
+            self.labels.append(label)
+            self.points.append(Dataset.gaussianPoint(centroids[label],sigma))
+
 
     def get_points(self):
         return self.points
@@ -34,9 +53,27 @@ class Dataset:
             stddraw.point(point[0], point[1])
         stddraw.show()
 
+    def hypercube(d: int) -> list[list[int]]:
+        powers = [ 2**i for i in range(d) ]
+        vertices = []
+        val = lambda i,d: 1 if i & powers[d] else -1
+        for i in range(2**d):
+            vertices.append([ val(i,j) for j in range(d) ])
+        return vertices
+    
 
 if __name__ == "__main__":
+    n = 20
+
+    # Generar un dataset de n puntos en 3D
     ds = Dataset()
-    ds.generateClusters(20,2)
+    ds.generateClusters(n, 3, 8)
+    print(ds.get_points())
+
+    # Generar un dataset de n puntos en 2D
+    ds = Dataset()
+    ds.generateClusters2D(n)
     ds.plot_points()
+
+
 
