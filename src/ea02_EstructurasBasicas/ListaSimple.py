a55f3294ba6b41f7e9f59f1a0a4aa6516a5a8686
Jorge Londoño <jorge.londono@upb.edu.co>
2023-03-14 16:47:49 -0500

    Updates

--- src/ea02_EstructurasBasicas/ListaSimple.py
+++ src/ea02_EstructurasBasicas/ListaSimple.py
@@ -1,25 +1,50 @@
 
 
 class Nodo:
+    """Representacion de nodos de la lista simplemente enlazada
+    
+    Atributos
+    ---------
+    _item : Dato almacenado en el nodo
+    _next : Referencia al siguiente nodo de la lista    
+    """
 
     def __init__(self, item, next):
+        """Constructor de nodo, acepta un item y el nodo siguiente"""
         self._item = item
         self._next = next
 
 
 class ListaSimple:
+    """Clase que representa una lista simplemente enlazada.
+    
+    Atributos
+    ---------
+    _head : La cabeza de la lista.
+
+    Metodos
+    -------
+    add : Agrega un item a la lista.
+    __iter__ : Devolver el iterador de la lista
+    __next__ : Obtener el siguiente elemento del iterador
+
+    """
 
     def __init__(self):
+        """Constructor de la lista simple, inicializada como lista vacia"""
         self._head = None
 
     def add(self, item):
+        """Agregar un item en la lista simple"""
         self._head = Nodo(item, self._head)
 
     def __iter__(self):
+        """Obtener el iterador de la lista simple"""
         self._current = self._head
         return self
 
     def __next__(self):
+        """Obtener el siguiente item del iterador"""
         if self._current is not None:
             item = self._current._item
             self._current = self._current._next
